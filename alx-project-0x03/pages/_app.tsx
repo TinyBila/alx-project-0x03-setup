12cea7bd1654aba698c22312edb1a44d4f46b735
Tiny <129532205+TinyBila@users.noreply.github.com>
2025-09-28 20:45:23 +0200

    Update _app.tsx

--- alx-project-0x03/pages/_app.tsx
+++ alx-project-0x03/pages/_app.tsx
@@ -4,3 +4,14 @@ import type { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
+import type { AppProps } from 'next/app'
+import Layout from '@/components/layouts/Layout'
+import '@/styles/globals.css'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
+}
